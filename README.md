7ecca019c037819d734aa8491e756fc0e9aab7d2
Avinash <avinash@qxf2.com>
2019-01-09 10:57:21 +0530

    Adding a simple flask app

---
+++ README.md
@@ -0,0 +1,1 @@
+# heroku Flask APP
---
+++ app.py
@@ -0,0 +1,11 @@
+# Script to testing Flask app in Heroku
+from flask import Flask
+app = Flask(__name__)
+
+ 
+@app.route("/")
+def hello():
+    return "Hello Avinash!"
+ 
+if __name__ == "__main__":
+    app.run()
